39bf1ca7b570bdaee470929c3166d902531382e2
Yoonie Kim <aayuooh@gmail.com>
2022-10-04 12:46:06 -0400

    adding our dependency lock file

---
+++ build/precache-manifest.5e031bf21fb46581fca654f0a46e4caa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5584aec0bfe5f9d36b38",
+    "url": "/aayuoohkim/COMP3104/static/css/main.37eb7437.chunk.css"
+  },
+  {
+    "revision": "5584aec0bfe5f9d36b38",
+    "url": "/aayuoohkim/COMP3104/static/js/main.5584aec0.chunk.js"
+  },
+  {
+    "revision": "27dd1d808a736a6594f9",
+    "url": "/aayuoohkim/COMP3104/static/js/runtime~main.27dd1d80.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/aayuoohkim/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "7b1c153d9601cb1af155fc15d39f3d50",
+    "url": "/aayuoohkim/COMP3104/index.html"
+  }
+];
